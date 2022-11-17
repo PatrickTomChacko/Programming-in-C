7116b6e2a7fbae7292d2874b4c55e2c5bd26a20f
Manya Singh <50653347+manya09singh@users.noreply.github.com>
2022-11-17 13:46:44 +0000

    adding comments to the code "hanoi.c"

--- Practical10/hanoi.c
+++ Practical10/hanoi.c
@@ -11,8 +11,10 @@ void hanoi (int n, int source, int destination, int inter)
     }
     else
     {
+        // applying the function recursively for (n-1) discs from source to inter
         hanoi(n-1,source,inter,destination);
         printf("Move disc from %d to %d\n",source, destination);
+        // applying the function recursively for (n-1) discs from inter to destination
         hanoi(n-1,inter,destination,source);
     }
 
